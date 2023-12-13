2593a43271ea2183d9a2086f995dc95ad7fe43e8
time1y <13487958@qq.com>
2023-12-13 19:07:44 +0800

    time1y

--- public/anzhiyu/random.js
+++ public/anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/11/Front-matter 的基本认识/","2023/11/11/md语法/"];function toRandomPost(){
+var posts=["2023/11/11/md语法/","2023/11/11/Front-matter 的基本认识/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
